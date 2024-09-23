f734388e7d052362f6621058364dad5a43490db0
Lucas Bruder <110999721+Bruder1z@users.noreply.github.com>
2024-09-23 14:34:52 -0300

    Add files via upload

---
+++ sql/SQLQuery19.sql
@@ -0,0 +1,21 @@
+--booleanos
+--bit
+
+
+--caracteres
+--char
+
+--numeros
+--valores exatos (tinyint)(smallint)(int)(bigint)
+--(numeric) (decimal)
+--valores aproximados
+--REAL - aproximado até 15 digitos
+--float -mesmo conceito do real
+
+--TEMPORAIS
+--DATE aaaa/mm/dd
+--DATETIME armazena data e horas aaaa/mm/dd:hh:mm:ss
+--DATETIME2 
+--SMALLDATETIME dentro de um limite especifico
+--datetimeoffset - data e hora com fuso horario
+
